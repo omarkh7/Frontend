7ec92de7c50a9f7db30f3815c58832893e84667d
Omar Khankan <omar.khankan@gmail.com>
2023-01-29 21:02:24 +0200

    Creating Components

---
+++ src/Components/Acknowledgments/Acknowledgments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Acknowledgments() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Acknowledgments
---
+++ src/Components/Writeups/Writeups.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Writeups() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Writeups
