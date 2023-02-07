1e5e0872fe82f8b284d134a9cf937ac68dc60c76
Assala <ismailassala1@gmail.com>
2023-02-08 01:34:00 +0200

    Home page and Biography

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
+
+// import Biography from './Components/Biography/Biography.jsx';
+
+// import Blogs from './Components/Blogs/Blogs.jsx';
+// import './Components/Certification/Certification'
+
+// import Modals from './Components/BioHomepage/BioHomepage';
+
+
+// import Home from './Components/home';
+import AllCertifications from './Components/Certification/Certification';
+import AllHighlights from './Components/Highlights/Highlights';
+import HomePage from './Components/Home/Home';
+import AllWriteups from './Components/Writeups/Writeups';
+
 
 function App() {
   return (
     <div >
-Hello Team :p   </div>
+      {/* <Biography/> */}
+      {/* <Blogs/>  */}
+      
+{/* <Modals />  */}
+
+{/* <PostBlog/> */}
+
+{/* <Achievements/>
+<Acknowledgments/> */}
+{/* <Certification/> */}
+{/* <Highlights/>
+<Writeups/> */}
+
+<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/Certification" element={<AllCertifications/>}></Route>
+      <Route path="/Highlights" element={<AllHighlights/>}></Route>
+      <Route path="/Writeups" element={<AllWriteups/>}></Route>
+    </Routes>
+  </BrowserRouter>
+
+  {/* <HomePage/> */}
+      </div>
   );
 }
 
--- src/Components/Writeups/Writeups.jsx
+++ src/Components/Writeups/Writeups.jsx
@@ -1,11 +1,67 @@
 import React from 'react'
+import "./Writeups.css"
+import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
+import { Link, useParams } from "react-router-dom";
 
-function Writeups() {
-  return (
-    <div>
+
+function AllWriteups() {
+
+  const [alldata, setAllData] = useState([]);
+
+  const apiURL = 'http://localhost:5000/info';
+
+
+  const fetchallData = async () => {
+    try {
+      const response = await axios.get(apiURL);
+      console.log(response.data);
+      setAllData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+
+    fetchallData();
+
+  }, []);
+
+
+  return(
+  <div className="Writeupss">
+  <p className="title">Writeups</p>
+  <div className="containerWriteups">
+  {alldata.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)).filter(info => info.info_category[0] === 'Writeups').map((info, index) => (
+<div key={info._id}>
+    <div className="row2">
+        
+    <img className="imeg" src={`http://localhost:5000/${info.info_image}`} alt="img"/>
+        
+        <div className="shadow1"></div>
+        <div className="textwidget">
+          <p>{info.info_title}</p>
+        <p>{info.info_description}</p>
+        
+         
+        </div>
+        <a className="ReadFullWriteup" href={info.info_url}>Read Full Writeup</a>
+          
       
     </div>
-  )
+
+
+</div>
+))}
+
+
+
+</div>
+</div>
+
+
+)
 }
 
-export default Writeups
+export default AllWriteups
